974aebdab51549ff7ad730050670ea7b6aa36c10
Phoebe Lau <phoebelau7@gmail.com>
2018-02-02 02:26:49 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,6 +1,6 @@
 module ArtistsHelper
   def display_artist(artist)
-    if self.name.present?
+    if artist.name.present?
       link_to artist_name, artist_path(artist)
     else
       link_to "Add Artist", edit_song_path(song)
